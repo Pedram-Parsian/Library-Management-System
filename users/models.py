8d2fb75bc4eda1bcad38a62535bd84ab0ddef1de
Pedram Parsian <Pedram_Parsian@outlook.com>
2019-09-20 09:33:13 +0430

    Complete user model; add EmailActivation

--- users/models.py
+++ users/models.py
@@ -1,8 +1,15 @@
 import hashlib
 import urllib
+from datetime import timedelta
 
+from django.conf import settings
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
+from django.core.mail import send_mail
+from django.db.models import Q
+from django.template.loader import get_template
+from django.utils import timezone
+from django.urls import reverse
 from django.db import models
 
 
@@ -36,10 +43,10 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractUser):
+    # todo user should be notified if there is any new notification!
     username = None
     email = models.EmailField('email address', unique=True)
     email_activated = models.BooleanField('user\'s email confirmation', default=False)
-    notifications = models.ManyToManyField('Notification', verbose_name='user\'s notifications', blank=True)
     avatar = models.ImageField(upload_to='user-avatars/', blank=True, null=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']
@@ -56,9 +63,95 @@ class User(AbstractUser):
         gravatar_size = urllib.parse.urlencode({'d': 'identicon', 's': '35'})
         return f'https://www.gravatar.com/avatar/{encoded_email_address}?{gravatar_size}'
 
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} ({self.email})'
+
 
 class Notification(models.Model):
-    # todo Complete this class :)
-    pass
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=settings.CHARFIELD_MAX_LENGTH)
+    text = models.TextField(max_length=settings.TEXTFIELD_MAX_LENGTH)
 
-# todo add EmailActivationClass
+    def __str__(self):
+        return f'<{self.title}> {self.text} for <{self.user}>'
+
+
+class EmailActivationQuerySet(models.query.QuerySet):
+    def confirmable(self):
+        # todo with this custom QuerySet, we can call EmailActivation.objects.all().confirmable() and get all confirmable objects,so, we can have "unactivated accounts & count of them" in the admin using this method!
+        start_range = timezone.now() - timedelta(days=settings.DEFAULT_ACTIVATION_DAYS or 7)
+        end_range = timezone.now()
+        return self.filter(
+            is_activated=False,
+            forced_expired=False,
+        ).filter(
+            timestamp__gt=start_range,
+            timestamp__lte=end_range,
+        )
+
+
+class EmailActivationManager(models.Manager):
+    def get_queryset(self):
+        return EmailActivationQuerySet(self.model, using=self._db)
+
+    def confirmable(self):
+        return self.get_queryset().confirmable()
+
+    def email_exists(self, email):
+        return self.get_queryset().filter(Q(email=email) | Q(user__email=email)).filter(is_activated=False)
+
+
+class EmailActivation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    email = models.EmailField()
+    key = models.CharField(max_length=120, null=True, blank=True)
+    is_activated = models.BooleanField(default=False)
+    forced_expired = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+
+    objects = EmailActivationManager()
+
+    def __str__(self):
+        return f'[{self.is_activated}] {self.email}'
+
+    def can_activate(self):
+        qs = EmailActivation.objects.filter(pk=self.pk).confirmable()
+        if qs.exists():
+            return True
+        return False
+
+    def activate(self):
+        if self.can_activate():
+            user = self.user
+            user.email_activated = True
+            user.save()
+            # todo Post activation user signal -> send another email (welcome email! + offer)
+            self.is_activated = True
+            self.save()
+            return True
+        return False
+
+    def regenerate(self):
+        self.key = None
+        self.save()
+        if self.key is not None:
+            return True
+        return False
+
+    def send_activation_email(self):
+        if not self.is_activated and not self.forced_expired:
+            if self.key:
+                path = settings.BASE_URL + reverse('email_activation', kwargs={'key': self.key})
+                context = {
+                    'path': path,
+                    'email': self.email,
+                }
+                txt_ = get_template('users/emails/confirm_email.txt').render(context)
+                html_ = get_template('users/emails/confirm_email.html').render(context)
+                subject = 'Confirm Your Email'
+                from_email = settings.DEFAULT_FROM_EMAIL
+                recipient_list = [self.email]
+                sent_mail = send_mail(subject, txt_, from_email, recipient_list, html_message=html_, fail_silently=False)
+                return sent_mail
+        return False
