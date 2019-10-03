cc4225ad2310d92d125b052ef4bb679478520c8d
Pedram Parsian <Pedram_Parsian@outlook.com>
2019-10-03 13:54:06 +0330

    fix error in calling CBV

--- documents/urls.py
+++ documents/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 app_name = 'documents'
 urlpatterns = [
-    path('list/', views.DocumentListView, name='list')
+    path('list/', views.DocumentListView.as_view(), name='list')
 ]
