a55cb242c2b831a914694e2ca03634f028370526
Pedram Parsian <Pedram_Parsian@outlook.com>
2019-10-03 14:00:31 +0330

    Add inital admin for documents

--- documents/admin.py
+++ documents/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+admin.site.register(models.Document)
+admin.site.register(models.DocumentType)
+admin.site.register(models.Author)
+admin.site.register(models.Translator)
+admin.site.register(models.Language)
+admin.site.register(models.Publication)
+admin.site.register(models.AgeClassification)
+admin.site.register(models.Building)
+admin.site.register(models.Comment)
+admin.site.register(models.Location)
+admin.site.register(models.Floor)
