e4a94fc0036977b5276ae2e2d315281b5fc4c4ed
Pedram Parsian <Pedram_Parsian@outlook.com>
2019-10-04 21:51:35 +0330

    add get_gravatar_url func to utilities

--- lms/utilities.py
+++ lms/utilities.py
@@ -23,6 +23,12 @@ def random_string_generator(size=10, chars=string.ascii_lowercase + string.digit
     return ''.join(random.choice(chars) for _ in range(size))
 
 
+def get_gravatar_url(email):
+    encoded_email_address = hashlib.md5(email.lower().encode('utf-8')).hexdigest()
+    gravatar_size = urllib.parse.urlencode({'d': 'identicon', 's': '35'})
+    return f'https://www.gravatar.com/avatar/{encoded_email_address}?{gravatar_size}'
+
+
 def convert_range(old_min, old_max, new_min, new_max, value):
     old_range = old_max - old_min
     new_range = new_max - new_min
