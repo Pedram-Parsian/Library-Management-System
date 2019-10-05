1b8fd3f0897fc559060e3ca3b20dc9efd3a65241
Pedram Parsian <Pedram_Parsian@outlook.com>
2019-10-05 06:50:59 +0330

    add navbar variable to get_context_data of DocumentListView

--- documents/views.py
+++ documents/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.views.generic import ListView
 
 from . import models
@@ -7,3 +6,8 @@ from . import models
 class DocumentListView(ListView):
     template_name = 'documents/list.html'
     model = models.Document
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['navbar'] = 'documents'
+        return context
