2bf3e2fd81b913945da9f4379b6a1f34b5131fb3
Pedram Parsian <Pedram_Parsian@outlook.com>
2019-10-09 13:13:41 +0330

    add all models to admin

--- users/admin.py
+++ users/admin.py
@@ -3,3 +3,7 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.User)
+admin.site.register(models.Member)
+admin.site.register(models.Membership)
+admin.site.register(models.Notification)
+admin.site.register(models.EmailActivation)
