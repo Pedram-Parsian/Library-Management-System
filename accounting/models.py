31fd624c92fc5e1c263d3d02b890eb6160e91a44
Pedram Parsian <Pedram_Parsian@outlook.com>
2019-10-09 13:54:05 +0330

    initial model for accounting

--- accounting/models.py
+++ accounting/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Payment(models.Model):
+    CASH = 10
+    CREDIT = 20
+    BALANCE = 30
+
+    PAYMENT_CHOICES = (
+        (CASH, 'Cash'),
+        (CREDIT, 'Credit Card'),
+        (BALANCE, 'Balance'),
+    )
+
+    # user = models.ForeignKey()
+    payment_type = models.IntegerField(choices=PAYMENT_CHOICES)
+    amount = models.IntegerField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.amount} Rial on {self.timestamp}'
