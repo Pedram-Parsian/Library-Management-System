cd619a82fdfe0fd5af779abe9a1266df034f300e
Pedram Parsian <Pedram_Parsian@outlook.com>
2019-10-09 18:31:18 +0330

    finalize the circulation models

--- circulation/models.py
+++ circulation/models.py
@@ -24,6 +24,10 @@ class Issue(models.Model):
     fines = models.ManyToManyField('Fine', blank=True)
 
     @property
+    def has_renews(self):
+        return True if self.renew_set else False
+
+    @property
     def is_returned(self):
         return True if self.returned_date else False
 
@@ -55,8 +59,20 @@ class Fine(models.Model):
 
 class Reserve(models.Model):
     user = models.ForeignKey(Member, on_delete=models.CASCADE)
-    documents = models.ForeignKey(Document, on_delete=models.CASCADE)
+    document = models.ForeignKey(Document, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
+    available_since = models.DateTimeField(blank=True, null=True)
+    # after the member has submitted the issue, it will be stored here:
+    issue = models.ForeignKey(Issue, blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.user} reserve {self.document} @ {self.timestamp}'
 
 
+class Renew(models.Model):
+    issue = models.ForeignKey(Issue, on_delete=models.PROTECT)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField()
 
+    def __str__(self):
+        return f'Renew for issue <{self.issue_id}> from {self.timestamp} to {self.due_date}'
