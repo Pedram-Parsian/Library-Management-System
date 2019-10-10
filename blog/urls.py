b79f398597ae3dc15f7ee97cfa2eed05127c271e
Pedram Parsian <Pedram_Parsian@outlook.com>
2019-10-10 19:30:55 +0330

    Change blog's home page name

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    path('', views.BlogHomeView.as_view(), name='blog'),
+    path('', views.BlogHomeView.as_view(), name='home'),
     path('like/<int:post_id>', views.LikePostView, name='like_post'),
     path('<slug:slug>/', views.BlogDetailView.as_view(), name='post'),
     re_path(r'^archive/(?P<year>[0-9]{4})-(?P<month>[0-9]{2})/$', views.BlogArchiveView.as_view(), name='archive'),
