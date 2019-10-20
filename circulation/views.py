5fcd2743883fa85f6bb3dc28821e1739a8fb299e
Pedram Parsian <Pedram_Parsian@outlook.com>
2019-10-20 21:20:13 +0330

    add LoginRequiredMixin and form_valid to add member to form before saving

--- circulation/views.py
+++ circulation/views.py
@@ -3,16 +3,23 @@ from django.urls import reverse_lazy
 from django.views.generic import FormView
 from bootstrap_modal_forms.generic import BSModalCreateView
 
+from users.models import Member
 from . import forms, models
 
 
-class ReserveView(BSModalCreateView):
+class ReserveView(LoginRequiredMixin, BSModalCreateView):
     template_name = 'circulation/reserve_modal.html'
     form_class = forms.ReserveForm
     success_message = 'The document was successfully reserved.'
     # todo change the success_url to member's profile page -> reserves section or
     #  document detail page with reserved (disabled) button:
     success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.member = Member.objects.get(user=self.request.user)
+        self.object.document_id = self.kwargs.get('id')
+        return super().form_valid(form)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
