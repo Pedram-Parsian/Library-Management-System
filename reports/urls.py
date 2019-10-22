9b5e3601612ac3d43452b9a61ca85a2662891c02
Pedram Parsian <Pedram_Parsian@outlook.com>
2019-10-22 23:47:09 +0330

    add basic url routing for reports app

--- reports/urls.py
+++ reports/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 
-urlpatterns = [
+from . import views
 
+
+app_name = 'reports'
+urlpatterns = [
+    path('members/', views.MembersList.as_view(), name='members_list'),
+    path('members/cards', views.MembersCardList.as_view(), name='members_card_list'),
+    path('members/cards/pdf', views.generate_pdf, name='members_card_list_pdf'),
 ]
