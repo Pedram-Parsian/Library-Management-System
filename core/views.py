d0bed9e29671315b8058ebc7e2fcf28bf162eec5
Pedram Parsian <Pedram_Parsian@outlook.com>
2019-10-23 11:29:15 +0330

    complete contact-us view, add navbar value to context data and load recent blog posts in home page

--- core/views.py
+++ core/views.py
@@ -1,15 +1,41 @@
+from django.contrib import messages
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, FormView
+from . import forms
 
 
 class HomeView(TemplateView):
     template_name = 'core/home.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['navbar'] = 'home'
+        # context['recent_posts'] = Post.objects.all()[:3]
+        return context
+
 
 class AboutView(TemplateView):
     template_name = 'core/about.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['navbar'] = 'about'
+        return context
 
-class ContactView(TemplateView):
+
+class ContactView(FormView):
     template_name = 'core/contact.html'
+    success_url = reverse_lazy('contact')
+    form_class = forms.ContactForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['navbar'] = 'CONTACT'
+        return context
+
+    def form_valid(self, form):
+        form.send_mail()
+        messages.success(self.request, 'Your message has been sent. Thanks!')
+        return super(ContactView, self).form_valid(form)
 
