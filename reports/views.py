5972bb45517d47ef4ecb408ac1c852d853d1975a
Pedram Parsian <Pedram_Parsian@outlook.com>
2019-10-25 13:11:17 +0330

    complete single card generation

--- reports/views.py
+++ reports/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.template.loader import render_to_string
 from django.views.generic import ListView, DetailView
+from django.views.generic.detail import BaseDetailView
 
 from circulation.models import Issue, Reserve, Renew
+from reports.utilities import generate_single_card
 from users.models import Member
 from documents.models import Document
 
@@ -17,12 +19,17 @@ class DocumentsReport(ListView):
     model = Document
 
 
-class MemberCard(DetailView):
+class MemberCard(BaseDetailView):
     # just return a pdf
     model = Member
 
     def get(self, request, *args, **kwargs):
-        ...
+        member_id = self.kwargs.get('pk')
+        path_to_pdf = generate_single_card(member_id)
+        with open(path_to_pdf, 'rb') as member_card:
+            response = HttpResponse(member_card.read(), content_type='application/pdf')
+            response['Content-Disposition'] = f'inline;filename=member_{member_id}_card.pdf'
+            return response
 
 
 class MemberReport(DetailView):
