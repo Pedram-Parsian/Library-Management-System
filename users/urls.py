3d25a38b4c658e81effc4aefac187dd7c2136da1
Pedram Parsian <Pedram_Parsian@outlook.com>
2019-10-25 08:47:49 +0330

    add route for issues in profile

--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('profile/', views.ProfileView.as_view(), name='profile'),
     path('profile/comments', views.ProfileCommentsView.as_view(), name='comments'),
     path('profile/reserves', views.ProfileReservesView.as_view(), name='reserves'),
+    path('profile/issues', views.ProfileIssuesView.as_view(), name='issues'),
     path('profile/reserves/delete/<int:pk>', views.ProfileReserveDelete.as_view(), name='delete_reserve'),
     path('profile/comments/delete/<int:pk>', views.ProfileCommentDelete.as_view(), name='delete_comment'),
     path('logout/', views.UserLogoutView.as_view(), name='logout'),
@@ -14,6 +15,8 @@ urlpatterns = [
     path('signup/', views.SignupView.as_view(), name='signup'),
     path('signup-successful', views.SignupCompleteView.as_view(), name='signup_complete'),
     path('email-activation/resend/', views.ResendActivationEmail.as_view(), name='resend_activation_email'),
-    path('email-activation/resend-successful', views.ResendActivationEmailSuccessful.as_view(), name='resend_activation_email_successful'),
-    re_path(r'^email-activation/(?P<key>[0-9A-Za-z]+)/$', views.AccountEmailActivationView.as_view(), name='email_activation'),
+    path('email-activation/resend-successful', views.ResendActivationEmailSuccessful.as_view(),
+         name='resend_activation_email_successful'),
+    re_path(r'^email-activation/(?P<key>[0-9A-Za-z]+)/$', views.AccountEmailActivationView.as_view(),
+            name='email_activation'),
 ]
