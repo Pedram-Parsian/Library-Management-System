7c25e227f3e095d74bb6d3ccd774d3f1d5c9336f
Pedram Parsian <Pedram_Parsian@outlook.com>
2019-11-01 21:10:02 +0330

    change success_url after submitting reserve object and add type annotation

--- circulation/views.py
+++ circulation/views.py
@@ -3,22 +3,22 @@ from django.urls import reverse_lazy
 from django.views.generic import FormView
 from bootstrap_modal_forms.generic import BSModalCreateView
 
+from . import models
 from users.models import Member
-from . import forms, models
+from . import forms
 
 
 class ReserveView(LoginRequiredMixin, BSModalCreateView):
+    object: models.Reserve
     template_name = 'circulation/reserve_modal.html'
     form_class = forms.ReserveForm
     success_message = 'The document was successfully reserved.'
-    # todo change the success_url to member's profile page -> reserves section or
-    #  document detail page with reserved (disabled) button:
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('users:reserves')
 
     def form_valid(self, form):
         self.object = form.save(commit=False)
-        self.object.member = Member.objects.get(user=self.request.user)
         self.object.document_id = self.kwargs.get('id')
+        self.object.member = Member.objects.get(user=self.request.user)
         return super().form_valid(form)
 
     def get_context_data(self, **kwargs):
