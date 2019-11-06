921fcdbde0234e0878690b1411bfe409f7d1d728
Pedram Parsian <Pedram_Parsian@outlook.com>
2019-11-07 01:11:43 +0330

    complete ReplyForm to include attachments [check description]
    - add attachments field as multiple FileField
    - use validators to check attachments' extension, mime type and size
    - add custom clean method to check total attachments count
    - include request in form to check attachments count

--- ticketing/forms.py
+++ ticketing/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.conf import settings
+from .validators import form_file_size_validator, file_type_validator, file_extension_validator
 
 from .models import Ticket, Reply
 
@@ -12,6 +14,20 @@ class TicketForm(forms.ModelForm):
 
 
 class ReplyForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request')
+        super().__init__(*args, **kwargs)
+
+    attachments = forms.FileField(
+        widget=forms.FileInput(
+            attrs={'accept': ','.join(settings.VALID_ATTACHMENT_EXTENSIONS.keys()), 'multiple': True}), required=False,
+        validators=(file_extension_validator, file_type_validator, form_file_size_validator))
+
+    def clean_attachments(self):
+        if len(self.request.FILES.getlist('attachments')) > settings.MAX_ATTACHMENTS:
+            self.add_error('attachments', 'Too many files!')
+        return self.cleaned_data.get('attachments')
+
     class Meta:
         model = Reply
         fields = ['text']
