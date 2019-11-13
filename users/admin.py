f5e4653790bdd8c9e776e37adca14ac801ed6a15
Pedram Parsian <Pedram_Parsian@outlook.com>
2019-11-13 23:19:29 +0330

    add list_display to MemberAdmin to also show their identification_codee and membership

--- users/admin.py
+++ users/admin.py
@@ -9,7 +9,10 @@ class UserAdmin(admin.ModelAdmin):
 
 @admin.register(models.Member)
 class MemberAdmin(admin.ModelAdmin):
-    ...
+    list_display = ['__str__', 'get_member_identification_code', 'membership']
+
+    def get_member_identification_code(self, member):
+        return member.user.identification_code
 
 
 @admin.register(models.Membership)
