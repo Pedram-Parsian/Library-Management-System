b392d21697ab67dc6f4958f412c80de91950631f
Pedram Parsian <Pedram_Parsian@outlook.com>
2019-11-14 01:38:21 +0330

    add methods for django admin list_display usage [check description]
    - add get_documents_count on BasePerson model
    - add get_documents_count on DocumentType model
    - add get_documents_count on Language model
    - add get_floors_count on Building model
    - add get_repositories_count on Floor model
    - add get_racks_count on Repository
    - add get_rows_count on Rack model
    - add get_documents_count on Row model
    - add get_documents_count on Publisher model
    - add get_documents_count on AgeClassification model

--- documents/models.py
+++ documents/models.py
@@ -9,6 +9,9 @@ from users.models import Member
 class BasePerson(models.Model):
     name = models.CharField(max_length=settings.CHARFIELD_MAX_LENGTH)
 
+    def get_documents_count(self):
+        return self.document_set.count()
+
     def __str__(self):
         return self.name
 
@@ -106,6 +109,9 @@ class DocumentType(models.Model):
     title = models.CharField(max_length=settings.CHARFIELD_MAX_LENGTH)
     is_digital = models.BooleanField(default=False)
 
+    def get_documents_count(self):
+        return self.document_set.count()
+
     def __str__(self):
         return f'{self.title} ({"digital" if self.is_digital else "physical"})'
 
@@ -113,6 +119,9 @@ class DocumentType(models.Model):
 class Language(models.Model):
     title = models.CharField(max_length=settings.CHARFIELD_MAX_LENGTH)
     native_name = models.CharField(max_length=settings.CHARFIELD_MAX_LENGTH, blank=True, null=True)
+
+    def get_documents_count(self):
+        return self.document_set.count()
 
     def __str__(self):
         if self.native_name:
@@ -135,6 +144,9 @@ class Editor(BasePerson):
 class Building(models.Model):
     title = models.CharField(max_length=settings.CHARFIELD_MAX_LENGTH)
 
+    def get_floors_count(self):
+        return self.floor_set.count()
+
     def __str__(self):
         return f'Building {self.title}'
 
@@ -142,6 +154,9 @@ class Building(models.Model):
 class Floor(models.Model):
     building = models.ForeignKey(Building, on_delete=models.PROTECT)
     title = models.CharField(max_length=settings.CHARFIELD_MAX_LENGTH)
+
+    def get_repositories_count(self):
+        return self.repository_set.count()
 
     def __str__(self):
         return f'Floor {self.title}'
@@ -151,6 +166,9 @@ class Repository(models.Model):
     floor = models.ForeignKey(Floor, on_delete=models.PROTECT)
     title = models.CharField(max_length=settings.CHARFIELD_MAX_LENGTH)
 
+    def get_racks_count(self):
+        return self.rack_set.count()
+
     def __str__(self):
         return f'Repository {self.title}'
 
@@ -159,6 +177,9 @@ class Rack(models.Model):
     repository = models.ForeignKey(Repository, on_delete=models.PROTECT)
     title = models.CharField(max_length=settings.CHARFIELD_MAX_LENGTH)
 
+    def get_rows_count(self):
+        return self.row_set.count()
+
     def __str__(self):
         return f'Rack {self.title}'
 
@@ -166,6 +187,9 @@ class Rack(models.Model):
 class Row(models.Model):
     rack = models.ForeignKey(Rack, on_delete=models.PROTECT)
     title = models.CharField(max_length=settings.CHARFIELD_MAX_LENGTH)
+
+    def get_documents_count(self):
+        return self.document_set.count()
 
     def __str__(self):
         return f'Row {self.title}'
@@ -176,6 +200,9 @@ class Publisher(models.Model):
     date_of_establishment = models.DateField(blank=True, null=True)
     refer_to = models.ForeignKey('self', blank=True, null=True, on_delete=models.PROTECT)
 
+    def get_documents_count(self):
+        return self.document_set.count()
+
     def __str__(self):
         if self.refer_to:
             return f'{self.name} ({self.refer_to.name})'
@@ -185,6 +212,9 @@ class Publisher(models.Model):
 class AgeClassification(models.Model):
     title = models.CharField(max_length=settings.CHARFIELD_MAX_LENGTH)
     min_age = models.PositiveSmallIntegerField()
+
+    def get_documents_count(self):
+        return self.document_set.count()
 
     def __str__(self):
         return f'{self.title} (from {self.min_age} years old)'
