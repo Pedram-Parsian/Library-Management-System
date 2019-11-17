0a7cceabfec2189f6923b3995d8a8e625d155b30
Pedram Parsian <Pedram_Parsian@outlook.com>
2019-11-17 15:04:58 +0330

    fix Meta class (remove it) and add 'make_published' action for posts

--- blog/admin.py
+++ blog/admin.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
+from django.utils import timezone
+
 from . import models
 
 
 @admin.register(models.Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['__str__', 'slug']
+    list_display = ['title', 'get_status_display']
+    actions = ['make_published']
 
-    class Meta:
-        model = models.Post
+    def make_published(self, request, queryset):
+        posts_updated = queryset.update(status=models.Post.PUBLISHED, date_published=timezone.now())
+        message = '1 post was' if posts_updated == 1 else f'{posts_updated} posts were'
+        self.message_user(request, f'{message} successfully marked as published.')
+
+    make_published.short_description = 'Mark selected posts as published'
 
 
 @admin.register(models.PostCategory)
