41480fe32eddb07de6208ae04106659caedabcfa
Pedram Parsian <Pedram_Parsian@outlook.com>
2019-11-20 21:59:53 +0330

    write tests for add, change, delete and  history of post

--- blog/tests/test_admin.py
+++ blog/tests/test_admin.py
@@ -2,6 +2,8 @@ from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
+from blog.models import Post
+
 
 class AdminSiteTest(TestCase):
 
@@ -15,9 +17,43 @@ class AdminSiteTest(TestCase):
         )
         self.client.force_login(self.admin_user)
 
-    def test_admin_posts_list_page_works(self):
-        """Test that the posts list page in the admin renders correctly"""
+        self.post = Post.objects.create(
+            title='Test Post',
+            status=Post.DRAFT,
+            body='Just testing...'
+        )
+
+    def test_admin_posts_changelist_page_works(self):
+        """Test that the posts changelist page in the admin renders correctly"""
         url = reverse('admin:blog_post_changelist')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_admin_posts_add_page_works(self):
+        """Test that the posts add page in the admin renders correctly"""
+        url = reverse('admin:blog_post_add')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_admin_posts_history_page_works(self):
+        """Test that the posts history page in the admin renders correctly"""
+        url = reverse('admin:blog_post_history', args=[self.post.pk])
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_admin_posts_delete_page_works(self):
+        """Test that the posts delete page in the admin renders correctly"""
+        url = reverse('admin:blog_post_delete', args=[self.post.pk])
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_admin_posts_change_page_works(self):
+        """Test that the posts change page in the admin renders correctly"""
+        url = reverse('admin:blog_post_change', args=[self.post.pk])
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, 200)
