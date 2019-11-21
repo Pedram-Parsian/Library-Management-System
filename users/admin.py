efb3ee13e127566f504ca35bac3d868e8fa97a3d
Pedram Parsian <Pedram_Parsian@outlook.com>
2019-11-21 16:44:56 +0330

    add action for generating membership card for MemberAdmin

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
 from . import models
 
 
@@ -10,9 +13,19 @@ class UserAdmin(admin.ModelAdmin):
 @admin.register(models.Member)
 class MemberAdmin(admin.ModelAdmin):
     list_display = ['__str__', 'get_member_identification_code', 'membership']
+    actions = ['generate_membership_card']
 
     def get_member_identification_code(self, member):
         return member.user.identification_code
+
+    get_member_identification_code.short_description = 'Identification Code'
+
+    def generate_membership_card(self, request, queryset):
+        selected = queryset.values_list('pk', flat=True)
+        return HttpResponseRedirect(
+            reverse('reports:members_cards', kwargs={'ids': ",".join(str(pk) for pk in selected)}))
+
+    generate_membership_card.short_description = 'Generate membership cards for selected members'
 
 
 @admin.register(models.Membership)
