24ccdf83a53f62139d00c9fcae3abeec00cf08b2
Pedram Parsian <Pedram_Parsian@outlook.com>
2019-11-21 16:53:30 +0330

    set date_hierarchy for all model admins to timestamp field

--- circulation/admin.py
+++ circulation/admin.py
@@ -6,21 +6,25 @@ from . import models
 class IssueAdmin(admin.ModelAdmin):
     list_display = ['document', 'member', 'timestamp', 'status']
     raw_id_fields = ['document', 'member']
+    date_hierarchy = 'timestamp'
 
 
 @admin.register(models.Fine)
 class FineAdmin(admin.ModelAdmin):
     list_display = ['issue', 'amount', 'timestamp']
     raw_id_fields = ['issue']
+    date_hierarchy = 'timestamp'
 
 
 @admin.register(models.Reserve)
 class ReserveAdmin(admin.ModelAdmin):
     list_display = ['member', 'document', 'status']
     raw_id_fields = ['document', 'member']
+    date_hierarchy = 'timestamp'
 
 
 @admin.register(models.Renew)
 class RenewAdmin(admin.ModelAdmin):
     list_display = ['issue', 'due_date']
     raw_id_fields = ['issue']
+    date_hierarchy = 'timestamp'
