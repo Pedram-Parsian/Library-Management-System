c693a7d0eba35b69187a5b9825562fc982abaa39
Pedram Parsian <Pedram_Parsian@outlook.com>
2019-11-26 21:08:44 +0330

    add 'test_unpublished_posts_are_accessible_for_superusers' test

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -1,4 +1,5 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
 from django.urls import reverse
 
 from blog.models import Post
@@ -14,3 +15,23 @@ class TestViews(TestCase):
         url = reverse('blog:post', kwargs={'slug': unpublished_post.slug})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
+
+    def test_unpublished_posts_are_accessible_for_superusers(self):
+        unpublished_post = Post.objects.create(
+            title='Sample Draft Post',
+            status=Post.DRAFT,
+            body='This post has not been published yet!'
+        )
+        url = reverse('blog:post', kwargs={'slug': unpublished_post.slug})
+
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            username='TestAdmin',
+            password='qazwsx',
+            email='admin@example.com',
+            gender=get_user_model().MALE,
+        )
+        self.client.force_login(self.admin_user)
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
