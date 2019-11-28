474d81fbcdcb75aa2e9894e7041b05fa7a1e1330
Pedram Parsian <Pedram_Parsian@outlook.com>
2019-11-28 21:48:02 +0330

    add top_ten_documents as context for HomeView

--- core/views.py
+++ core/views.py
@@ -2,6 +2,8 @@ from django.contrib import messages
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, FormView
+
+from documents.models import Document
 from . import forms
 
 
@@ -11,6 +13,7 @@ class HomeView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['navbar'] = 'home'
+        context['top_ten_documents'] = Document.get_top_ten_documents()
         # context['recent_posts'] = Post.objects.all()[:3]
         return context
 
@@ -38,4 +41,3 @@ class ContactView(FormView):
         form.send_mail()
         messages.success(self.request, 'Your message has been sent. Thanks!')
         return super(ContactView, self).form_valid(form)
-
